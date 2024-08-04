24fc0558dce75629ec2a6e5d8f4126d7d2fbff57
Mubina <mubinayigitaliyeva00@gmail.com>
2024-08-04 15:20:42 +0500

    wrote down query for migrations up

---
+++ migrations/000001_tables_up.down.sql
@@ -0,0 +1,15 @@
+-- Drop Medals table
+DROP TABLE IF EXISTS medals;
+
+-- Drop Athletes table
+DROP TABLE IF EXISTS athletes;
+
+-- Drop Events table
+DROP TABLE IF EXISTS events;
+
+-- Drop Countries table
+DROP TABLE IF EXISTS countries;
+
+-- Drop Users table
+DROP TABLE IF EXISTS users;
+
